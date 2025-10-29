d1d9576e6a6c5091ad221f4a3472e95d75eb0c91
Roni Efendi <ajoronicoding@gmail.com>
2025-10-29 19:05:30 +0700

    add websocet server file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+return [
+    'host' => '0.0.0.0',
+    'port' => 2096, // gunakan port SSL yang diizinkan
+    'ssl'  => true,
+    'cert' => '/etc/ssl/certs/ssl-cert.crt',
+    'key'  => '/etc/ssl/private/ssl-cert.key',
+    'secret' => 'ajoojek_active_ws'
+];
